33b49ca5c30d749204f498e654e12cf2af1d291a
Luis Casagrande <70987212+LuisBigHouse@users.noreply.github.com>
2024-10-29 21:08:45 -0300

    Create about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import { CustomHeader, CustomFooter } from '../components';
+
+const About = () => {
+    return (
+        <main className=' min-h-screen flex flex-col justify-between '>
+        <CustomHeader />
+        <div className='flex flex-col flex-grow'>
+ 
+        </div>
+        <CustomFooter />
+      </main>
+    );
+};
+export default About;
