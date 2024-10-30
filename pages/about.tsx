e0277533af4a90adc079b00353608cf9c17118ef
Luis Casagrande <70987212+LuisBigHouse@users.noreply.github.com>
2024-10-29 21:17:27 -0300

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,8 +5,7 @@ const About = () => {
         <main className=' min-h-screen flex flex-col justify-between '>
         <CustomHeader />
         <div className='flex flex-col flex-grow'>
-     //<script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
-    //<div class="elfsight-app-976078e4-7b79-47c5-8811-cc3be5c7dc7b" data-elfsight-app-lazy></div>
+
         </div>
         <CustomFooter />
       </main>
