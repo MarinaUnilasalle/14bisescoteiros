59fa4f5cfa81814183bedc35f53334972fa01cab
Matheus Espindola <espindola.matheus@hotmail.com>
2024-11-20 14:28:10 -0300

    git commit --amend

---
+++ pages/news.tsx
@@ -0,0 +1,17 @@
+import { CustomHeader, CustomFooter } from '../components';
+
+const News = () => {
+    return (
+        <main className=' min-h-screen flex flex-col justify-between '>
+        <CustomHeader />
+        <div className='flex flex-col flex-grow'>
+
+    <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+    <div className="elfsight-app-976078e4-7b79-47c5-8811-cc3be5c7dc7b" data-elfsight-app-lazy></div>
+
+        </div>
+        <CustomFooter />
+      </main>
+    );
+};
+export default News;
